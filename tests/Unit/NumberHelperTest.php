c757bb1a6a97ae8818c8a0d9c61d40ffcb242b4c
michael <michael@propaysystems.com>
2023-11-20 09:13:23 +0200

    Unit test cleanup
    Cleanup some of the tests.

--- tests/Unit/NumberHelperTest.php
+++ tests/Unit/NumberHelperTest.php
@@ -1,32 +1,34 @@
 <?php
 
+use PropaySystems\Utilities\Helpers\NumberHelper;
+
 it('can generate a random number', function () {
-    $value = \PropaySystems\Utilities\Helpers\NumberHelper::randomInt();
+    $value = NumberHelper::randomInt();
 
     expect($value)->toBeInt($value);
 });
 
 it('can generate percentage difference', function () {
-    $value = \PropaySystems\Utilities\Helpers\NumberHelper::getPercentageDifference(200, 100);
+    $value = NumberHelper::getPercentageDifference(200, 100);
 
-    expect($value['percentage'])->toBe((float) '50.0');
-    expect($value['direction'])->toBe('+');
+    expect($value['percentage'])->toBe((float) '50.0')
+        ->and($value['direction'])->toBe('+');
 });
 
 it('can generate percentage difference rounded', function () {
-    $value = \PropaySystems\Utilities\Helpers\NumberHelper::getPercentageDifference(200, 100);
+    $value = NumberHelper::getPercentageDifference(200, 100);
 
     expect($value['percentage'])->toBe((float) '50');
 });
 
 it('can format a number to more human-readable', function () {
-    $value = \PropaySystems\Utilities\Helpers\NumberHelper::numberFormat(100000);
+    $value = NumberHelper::numberFormat(100000);
 
     expect($value)->toBe('100.00K');
 });
 
 it('can format a number to more human-readable without precision', function () {
-    $value = \PropaySystems\Utilities\Helpers\NumberHelper::numberFormat(100000, 0);
+    $value = NumberHelper::numberFormat(100000, 0);
 
     expect($value)->toBe('100K');
 });
