8447fec8fa96cad7cded14da8945776998eaee12
MickProPay <MickProPay@users.noreply.github.com>
2023-11-20 07:13:56 +0000

    Fix styling

--- tests/Unit/FileHelperTest.php
+++ tests/Unit/FileHelperTest.php
@@ -4,25 +4,25 @@ use PropaySystems\Utilities\Helpers\FileHelper;
 
 dataset('file_sizes', [
 
-        'KB' => [
-            'input'    => '50000',
-            'expected' => '48.83 KB',
-        ],
+    'KB' => [
+        'input' => '50000',
+        'expected' => '48.83 KB',
+    ],
 
-        'MB' => [
-            'input'    => '5000000',
-            'expected' => '4.77 MB',
-        ],
+    'MB' => [
+        'input' => '5000000',
+        'expected' => '4.77 MB',
+    ],
 
-        'GB' => [
-            'input'    => '5000000000',
-            'expected' => '4.66 GB',
-        ],
+    'GB' => [
+        'input' => '5000000000',
+        'expected' => '4.66 GB',
+    ],
 
-        'TB' => [
-            'input'    => '10000000000000',
-            'expected' => '9.09 TB',
-        ],
+    'TB' => [
+        'input' => '10000000000000',
+        'expected' => '9.09 TB',
+    ],
 
 ]);
 
